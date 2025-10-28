bba4286db2e13da57175d32a53a0d8ba9d2d0a00
e1b23030 <e1b23030@oit.ac.jp>
2025-10-28 16:27:58 +0900

    SSEの実装

--- src/main/java/oit/is/hogeyama/hogehoge/team8_lec05/Team8Lec05Application.java
+++ src/main/java/oit/is/hogeyama/hogehoge/team8_lec05/Team8Lec05Application.java
@@ -2,7 +2,11 @@ package oit.is.hogeyama.hogehoge.team8_lec05;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableAsync
+@EnableScheduling
 @SpringBootApplication
 public class Team8Lec05Application {
 
